3a423a322022010e5d56380ccf5c0bf4407454ec
Rudransh Sharma <67827010+rudranshsharma123@users.noreply.github.com>
2021-09-19 21:13:34 +0530

    Update cliutils.py

--- Bokuno-Basketball-Backend/CLI/cliutils.py
+++ Bokuno-Basketball-Backend/CLI/cliutils.py
@@ -5,12 +5,15 @@ import os
 import requests
 from time import sleep
 # from untils.utils import datafile
-datafile = 'path of datafile'
+datafile = '/mnt/d/My projects/selfiehacks/jina/data/smol.csv'
 
 def clear_everything():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def type_writer_anim(textBlob):
+    """
+    Just a fancy way to printing out the texts
+    """
     for char in textBlob:
         print(char, end="", flush=True)
         sleep(0.02)
@@ -18,7 +21,9 @@ def type_writer_anim(textBlob):
 
 
 def get_ans(text):
-
+    '''
+    A simple function which goes to the Jina endpoint of the chatbot and deliveres the answeer to the question asked.
+    '''
     
     headers = {
         'Content-Type': 'application/json',
@@ -27,11 +32,11 @@ def get_ans(text):
     data = '{"top_k":1,"mode":"search","data":["' + text + '"]}'
 
     response = requests.post(
-        'http://0.0.0.0:12345/search', headers=headers, data=data)
+        'http://0.0.0.0:34567/search', headers=headers, data=data)
 
     res = response.json()
     return_text = res["data"]['docs'][0]['matches'][0]['tags']['ans']
-   
+    type_writer_anim(return_text)
     
     print('\n')
     
